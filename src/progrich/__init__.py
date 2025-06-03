e8ed6b7fd235c875ab123a083f2d4f23c9782ef0
Michael Jungo <michaeljungo92@gmail.com>
2025-06-03 17:20:32 +0200

    feat(table): add a table that displays the rows dynamically

--- src/progrich/__init__.py
+++ src/progrich/__init__.py
@@ -2,6 +2,7 @@ from . import columns, fmt
 from .manager import ManagedWidget, Manager
 from .pbar import ProgressBar
 from .spinner import Spinner
+from .table import Table
 from .widget import Widget
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "ManagedWidget",
     "ProgressBar",
     "Spinner",
+    "Table",
 ]
